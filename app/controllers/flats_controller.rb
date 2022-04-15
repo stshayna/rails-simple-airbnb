faff65d744b91ea7d6d11a181af314651e26f592
stshayna@gmail.com <stshayna@gmail.com>
2022-04-14 20:38:55 -0400

    create, edit, update

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :find_flat, only: %i[show]
+  before_action :find_flat, only: %i[show edit update create destroy]
 
   def index
     @flats = Flat.all
@@ -11,7 +11,30 @@ class FlatsController < ApplicationController
     @flat = Flat.new
   end
 
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :number_of_guests, :description, :price_per_night)
+  end
 
   def find_flat
     @flat = Flat.find(params[:id])
